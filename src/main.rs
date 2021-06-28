381cc99641b596c375874097e70766b924be8347
larryxiao <romerxzh@gmail.com>
2021-06-28 15:27:08 +0800

    [feature]:Guess game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use rand::prelude::*;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess Number");
+
+    println!("Please enter you guess number");
+
+    let mut guess = String::new();
+    println!("Read Line {}", io::stdin().read_line(&mut guess).expect("Fail read line"));
+    let guess: u32 = guess.trim().parse().expect("Parse error");
+
+    let mut rng = rand::thread_rng();
+    let secret: u32 = rng.gen_range(1..101);
+
+    match guess.cmp(&secret) {
+        Ordering::Equal => println!("You guess success {}", secret),
+        Ordering::Greater => println!("You guess is too big {}", secret),
+        Ordering::Less => println!("You guess is too small {}", secret),
+    }
 }
